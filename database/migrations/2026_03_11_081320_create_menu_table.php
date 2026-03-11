81f23b4279fb6135bad26d599630cd803c48897f
Hariz Razin <hariz-razin-bin-mohammad@webby.my>
2026-03-11 16:27:23 +0800

    feat: add menu GraphQL schema, mutations, and migration

---
+++ database/migrations/2026_03_11_081320_create_menu_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id('menu_id');
+            $table->String('menu_name');
+            $table->String('menu_slug')->unique();
+            $table->String('menu_description');
+            $table->decimal('menu_price_cents');
+            $table->string('menu_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu');
+    }
+};
